9e0bf775d37e384bcf2efb5de50e6be36a04f554
Rebecca Wise <astraea@well.com>
2020-12-28 21:05:41 +0000

    default sources to dos format; update image build-script convert file format of sources back and forth for convenience

--- build-image.sh
+++ build-image.sh
@@ -3,4 +3,18 @@ set -e
 ulimit -Sn 10000
 export NOREBO_ROOT=$PWD
 rm -rf build
+
+# ensure all Oberon source files have been converted to the correct format
+# before we begin the build...
+
+cd $1
+for x in *.Mod *.Tool ; do ${NOREBO_ROOT}/bin/dos2oberon $x $x ; done
+cd -
+
 python3 bin/build-image.py $1
+
+# Convert the files back to dos format for local editing...
+
+cd $1
+for x in *.Mod *.Tool ; do ${NOREBO_ROOT}/bin/oberon2dos $x $x ; done
+cd -
